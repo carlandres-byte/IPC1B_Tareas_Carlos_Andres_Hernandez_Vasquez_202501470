a29992da19c97ec211069ff1e2876b494132baa8
carlandres-byte <carlandres120@gmail.com>
2026-08-07 18:47:53 -0600

    feat: Estructura base del programa y declaracion de variables primitivas

---
+++ Tarea2/Main.java
@@ -0,0 +1,15 @@
+package Tarea2;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // Variables COntrol del sistema  
+        int tamanoArreglo = 5;
+        double limiteMaximo = 100.0;
+        char simboloSeparador = '=';
+        boolean ejecucionExitosa = true;
+
+        System.out.println("Programa iniciado correctamente.");
+    }
+}
