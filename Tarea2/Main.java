75c979bda52f41a2898b4daf1d143baca2d8e87c
carlandres-byte <carlandres120@gmail.com>
2026-08-07 18:51:00 -0600

    feat: Implementacion de la clase Scanner y metodo de validacion

--- Tarea2/Main.java
+++ Tarea2/Main.java
@@ -1,15 +1,40 @@
 package Tarea2;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+  
 
     public static void main(String[] args) {
-        // Variables COntrol del sistema  
         int tamanoArreglo = 5;
         double limiteMaximo = 100.0;
         char simboloSeparador = '=';
         boolean ejecucionExitosa = true;
 
-        System.out.println("Programa iniciado correctamente.");
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroPrueba = leerEnteroValidado(scanner, "Ingrese un numero entero (1-100): ", 1, 100);
+        System.out.println("Numero ingresado: " + numeroPrueba);
+    }
+
+    public static int leerEnteroValidado(Scanner scanner, String mensaje, int min, int max) {
+        int numero = 0;
+        boolean valido = false;
+
+        while (!valido) {
+            System.out.print(mensaje);
+            if (scanner.hasNextInt()) {
+                numero = scanner.nextInt();
+                if (numero >= min && numero <= max) {
+                    valido = true;
+                } else {
+                    System.out.println(" Error: El numero debe estar entre " + min + " y " + max + ".");
+                }
+            } else {
+                System.out.println(" Error: Debe ingresar un numero entero valido.");
+                scanner.next();
+            }
+        }
+        return numero;
     }
 }
